6d3f99c0505a6ce869b3438b24496cd93a4c7bc2
Apple Macintosh <jakkapong.sara@gmail.com>
2020-05-15 17:09:35 +0700

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
         <title>Laravel - @yield('title')</title>
         <link rel="manifest" href="{{asset('/manifest.webmanifest')}}">
+        <link rel="manifest" href="{{asset('/index.js')}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         @laravelPWA
@@ -68,36 +69,6 @@
             }
 
         </style>
-        <script>
-            let deferredPrompt;
-            const addBtn = document.querySelector('.add-button');
-            addBtn.style.display = 'none';
-
-            window.addEventListener('beforeinstallprompt', (e) => {
-                // Prevent Chrome 67 and earlier from automatically showing the prompt
-                e.preventDefault();
-                // Stash the event so it can be triggered later.
-                deferredPrompt = e;
-                // Update UI to notify the user they can add to home screen
-                addBtn.style.display = 'block';
-            
-                addBtn.addEventListener('click', (e) => {
-                // hide our user interface that shows our A2HS button
-                addBtn.style.display = 'none';
-                // Show the prompt
-                deferredPrompt.prompt();
-                // Wait for the user to respond to the prompt
-                deferredPrompt.userChoice.then((choiceResult) => {
-                    if (choiceResult.outcome === 'accepted') {
-                        console.log('User accepted the A2HS prompt');
-                    } else {
-                        console.log('User dismissed the A2HS prompt');
-                    }
-                    deferredPrompt = null;
-                    });
-                });
-            });
-        </script>
     </head>
     <body>
         @yield('content')        
