998a0e7179cc1b67c566dd62972a9d6cdd373417
Apple Macintosh <jakkapong.sara@gmail.com>
2020-05-16 12:28:19 +0700

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,28 +12,6 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     @laravelPWA
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="https://slot999.com/assets/images/apple-icon.png">
-    <link rel="icon" type="image/png" href="https://slot999.com/assets/images/favicon.png">
-    <title>สล็อต สล็อตออนไลน์ คาสิโนออนไลน์บนมือกว่า 100 เกม ได้ที่นี่ Slot999.com</title>
-    <meta name="description" content="เกมคาสิโน สล็อตออนไลน์ Slot XO Live22 แจ็คพ็อตแตกง่ายเริ่มต้นที่ 1 บาท พร้อมลุ้นรับเงินสด 100,000 บาท ทุกวัน รองรับทั้ง IOS และ Android ผู้ให้บริการเกมสล็อตออนไลน์อันดับ 1 ผ่านระบบอัตโนมัติ">
-    <meta name="keywords" content="สล็อต, สล็อตออนไลน์, สล๊อต, Slot, Slotxo, Live22, เกมออนไลน์, เกมคาสิโนออนไลน์, เกมสล็อต, เกมออนไลน์">
-    <meta name="author" content="slot999.com">
-    <meta property="og:url" content="https://slot999.com">
-    <meta property="og:type" content="games">
-    <meta property="og:title" content="สล็อต สล็อตออนไลน์ คาสิโนออนไลน์บนมือกว่า 100 เกม ได้ที่นี่ Slot999.com">
-    <meta property="og:description" content="เกมคาสิโน สล็อตออนไลน์ Slot XO Live22 แจ็คพ็อตแตกง่ายเริ่มต้นที่ 1 บาท พร้อมลุ้นรับเงินสด 100,000 บาท ทุกวัน รองรับทั้ง IOS และ Android ผู้ให้บริการเกมสล็อตออนไลน์อันดับ 1 ผ่านระบบอัตโนมัติ">
-    <meta property="og:locale" content="th_TH">
-    <meta property="og:type" content="game mobile">
-    <meta property="og:image" content="https://slot999.com/assets/images/thumbnail.png">
-    <meta name="theme-color" content="#da2429">
-    <script src='https://www.google.com/recaptcha/api.js' type="74f3c6db61e7884d386871d4-text/javascript"></script>
-    <link rel="stylesheet" href="https://slot999.com/assets/css/animate.min.css">
-    <link rel="stylesheet" href="https://slot999.com/assets/css/slot999.css">
-    <link rel="stylesheet" href="https://slot999.com/assets/css/all.min.css">
-
-
     <!-- Styles -->
     <style>
         html,
@@ -98,44 +76,7 @@
 
 <body>
     @yield('content')
-
-    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
-    <script src="https://slot999.com/assets/js/jquery.min.js" type="74f3c6db61e7884d386871d4-text/javascript"></script>
-    <script src="https://slot999.com/assets/js/popper.min.js" type="74f3c6db61e7884d386871d4-text/javascript"></script>
-    <script src="https://slot999.com/assets/js/bootstrap.min.js"
-        type="74f3c6db61e7884d386871d4-text/javascript"></script>
-    <script type="74f3c6db61e7884d386871d4-text/javascript">
-	$(function(){
-		$('#main-panel').load('https://slot999.com/Main/panel');
-		$('#main-nav').load('https://slot999.com/Main/nav');
-		$('#nav-mobile').load('https://slot999.com/Main/nav_mobile');
-
-					$('#nav-main').addClass('active');
-		
-    });
-	</script>
-    <script type="74f3c6db61e7884d386871d4-text/javascript">
-	function go(url,target = '_blank')
-	{
-		window.open(url,target);
-	}
-	</script>
-
-    <script type="74f3c6db61e7884d386871d4-text/javascript">
-var sc_project=12045380; 
-var sc_invisible=1; 
-var sc_security="546b1fa4"; 
-</script>
-    <script type="74f3c6db61e7884d386871d4-text/javascript" src="https://www.statcounter.com/counter/counter.js"
-        async></script>
-    <noscript>
-        <div class="statcounter"><a title="Web Analytics" href="https://statcounter.com/" target="_blank"><img
-                    class="statcounter" src="https://c.statcounter.com/12045380/0/546b1fa4/1/" alt="Web Analytics"></a>
-        </div>
-    </noscript>
-
-    <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js"
-        data-cf-settings="74f3c6db61e7884d386871d4-|49" defer=""></script>
+    
 </body>
 
 </html>
